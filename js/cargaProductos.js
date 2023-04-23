6c9ce48306fcf15f20e53767a27f63b7941a4b55
Mariano Mercevich <marianomercevich@gmail.com>
2023-04-23 18:03:37 -0300

    actualizacion

--- js/cargaProductos.js
+++ js/cargaProductos.js
@@ -1,23 +1,21 @@
 let listaProductos = [
-    {id:1, nombre:"ryzen 3 3200g", precio: 120000, descripcion: "Procesador de gama baja", cantidad: 1, img: 'assets/10.jpg', alt: "perro01"},
-    {id:2, nombre:"ryzen 3 3200x", precio: 130000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/02.jpg", alt: "imagen ryzen 3"},
-    {id:3, nombre:"ryzen 5 5600g", precio: 150000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/03.jpg", alt: "imagen ryzen 5"},
-    {id:4, nombre:"ryzen 5 5600x", precio: 160000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/04.jpg", alt: "imagen ryzen 5"},
-    {id:5, nombre:"ryzen 7 5700g", precio: 1750000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/05.jpg", alt: "imagen ryzen 7"},
-    {id:6, nombre:"ryzen 7 5800x", precio: 180000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/06.jpg", alt: "imagen ryzen 7"},
-    {id:7, nombre:"ryzen 9 5900x", precio: 400000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/07.jpg", alt: "imagen ryzen 9"},
-    {id:8, nombre:"ryzen 9 5800g", precio: 300000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/08.jpg", alt: "imagen ryzen 9"},
-    {id:9, nombre:"intel i3", precio: 90000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/09.jpg",alt: "imagen intel i3"},
-    {id:10,nombre: "intel i5", precio: 100000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/01.jpg",alt: "imagen intel i5"},
-    {id:11,nombre: "intel i7", precio: 150000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/11.jpg",alt: "imagen intel i7"},
-    {id:12,nombre: "intel i9", precio: 200000, descripcion: "Procesador de gama baja", cantidad: 1, img: "assets/12.jpg",alt: "imagen intel i9"}
+    {id:1, nombre:"Collares Pampa", precio: 1200, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: 'assets/10.jpg', alt: "perro10"},
+    {id:2, nombre:"Collares Marcel", precio: 1300, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/02.jpg", alt: "perro02"},
+    {id:3, nombre:"Collares Michi ", precio: 1500, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/03.jpg", alt: "perro03"},
+    {id:4, nombre:"Collares Michi II", precio: 1600, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/04.jpg", alt: "perro04"},
+    {id:5, nombre:"Collares Axolote", precio: 1700, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/05.jpg", alt: "perro05"},
+    {id:6, nombre:"Collares Silo", precio: 1800, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/06.jpg", alt: "perro06"},
+    {id:7, nombre:"Collares Mezcla", precio: 1900, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/07.jpg", alt: "perro07"},
+    {id:8, nombre:"Collares Coqueto", precio: 2000, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/01.jpg",alt: "perro01"},
+    {id:11,nombre: "Collares Cali", precio: 2500, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/11.jpg",alt: "perro11"},
+    {id:12,nombre: "Collares Gaucho", precio: 2100, descripcion: "Collar de cuero reciclado con tejido artesanal", cantidad: 1, img: "assets/12.jpg",alt: "perro12"}
 ]
 
-const arrEnFormatoJSON = JSON.stringify(listaProductos)
+ const arrEnFormatoJSON = JSON.stringify(listaProductos)
 
 localStorage.setItem("listaProductos",arrEnFormatoJSON)
 
-/*let listaProductos;
+/*
 let obtenerListaJSON = localStorage.getItem("listaProductos")
 
 if(obtenerListaJSON){
@@ -51,4 +49,3 @@ form.addEventListener("submit", (e) => {
 
     console.log(listaProductos)
 })
-*/
